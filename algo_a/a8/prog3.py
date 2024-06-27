23380d32781ff92a9cc080451b0df9d682cf9ae2
cijb-7724 <johokamia0@gmail.com>
2024-06-28 04:17:23 +0900

    kadai3

--- algo_a/a8/prog3.py
+++ algo_a/a8/prog3.py
@@ -1,9 +1,24 @@
-n = 9
-x = '□'
-y = '○'
-for i in range(n):
-    for j in range(i):
-        print(y, end='')
-    for j in range(n-i):
-        print(x, end='')
-    print(' ')
+a = [
+    [0.11, 0.21, 0.31],
+    [0.21, 0.22, 0.23],
+    [0.31, 0.32, 0.33]
+]
+
+b = [
+    [1.0, 2.0],
+    [3.0, 4.0],
+    [5.0, 6.0]
+]
+
+l = len(a)
+m = len(b)
+n = len(b[0])
+
+c = [[0] * n for _ in range(l)]
+
+for i in range(l):
+    for j in range(n):
+        for k in range(m):
+            c[i][j] += a[i][k] * b[k][j]
+
+print(c)
