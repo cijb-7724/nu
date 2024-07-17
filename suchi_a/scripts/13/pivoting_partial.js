a13703ca90189d7cc7ecd2c0e9205d76d4b3c211
cijb-7724 <johokamia0@gmail.com>
2024-07-17 22:11:33 +0900

    ch

--- suchi_a/scripts/13/pivoting_partial.js
+++ suchi_a/scripts/13/pivoting_partial.js
@@ -15,62 +15,57 @@ WScript.Echo("\n後退代入を実行します");
 backward_substitution(mat);
 
 // 部分ピボット選択を行いながら前進消去を行う関数
-function forward_elimination_2(A,flag){
-	for(var i=0;i<A.length-1;i++){
+function forward_elimination_2(A, flag) {
+    for (var i = 0; i < A.length - 1; i++) {
 
-		// A[j][i] (j=i,i+1,i+2,...)
-		// の中で一番絶対値の大きいような行番号jを見つける
-		//　そのような行番号を変数indexに保存する
-		var index = /*【1】ここを埋める*/;
-		var abs_max = /*【2】ここを埋める*/;
-		
-		for(var j=i+1;j<A.length;j++){
-			if(abs_max < /*【3】ここを埋める*/){
-				index = /*【4】ここを埋める*/;
-				abs_max = /*【5】ここを埋める*/;
-			}
-		}
-		
-		// 以下ではi行目とindex行目を入れ替える
-		// 以前実装した、行を交換する関数を使うとよい
-		// 行を交換する関数の定義もプログラムの下の方に書く（コピペしてくる）
-		/*【7】ここを埋める*/;
-		
-		for(var j=i+1;j<A.length;j++){
-			row_add(A,-A[j][i]/A[i][i],i,j);
-		}
-		if(flag==1){
-			WScript.Echo("\n"+(i+1)+"回目");
-			WScript.Echo((i+1)+"行目と"+
-						 (index+1)+"行目を入れ替えました");
-
-			print_matrix_3(A);
-		}
-	}
+        // A[j][i] (j=i,i+1,i+2,...)
+        // の中で一番絶対値の大きいような行番号jを見つける
+        //　そのような行番号を変数indexに保存する
+        var index = i; // 初期値としてi行目を設定
+        var abs_max = Math.abs(A[i][i]); // 初期値としてi行目の絶対値を設定
+        
+        for (var j = i + 1; j < A.length; j++) {
+            if (abs_max < Math.abs(A[j][i])) {
+                index = j;
+                abs_max = Math.abs(A[j][i]);
+            }
+        }
+        
+        // i行目とindex行目を入れ替える
+        row_swap(A, i, index);
+        
+        for (var j = i + 1; j < A.length; j++) {
+            row_add(A, -A[j][i] / A[i][i], i, j);
+        }
+        if (flag == 1) {
+            WScript.Echo("\n" + (i + 1) + "回目");
+            WScript.Echo((i + 1) + "行目と" + (index + 1) + "行目を入れ替えました");
+            print_matrix_3(A);
+        }
+    }
 }
 
-function backward_substitution(A){
-	var ret = [];
-	var row_num = A.length;
-	var col_num = A[0].length;
-	for(var i=row_num-1;i>=0;i--){
-		var tmp = /*【7】ここを埋める*/;
-		for(j=i+1;j<col_num-1;j++){
-			tmp -= /*【8】ここを埋める*/;
-		}
-		// a /= b は　a = a/b　と同じ意味
-		tmp /= /*【9】ここを埋める*/;
-		ret[i] = tmp;
-	}
-	var str = "(";
-	for(i=0;i<ret.length;i++){
-		str += ret[i];
-		if(i < ret.length-1) str += ",";
-		else str += ")";
-	}
-	WScript.Echo("解：");
-	WScript.Echo(str);
-	return ret;
+function backward_substitution(A) {
+    var ret = [];
+    var row_num = A.length;
+    var col_num = A[0].length;
+    for (var i = row_num - 1; i >= 0; i--) {
+        var tmp = A[i][col_num - 1]; // 最後の列がbの値
+        for (var j = i + 1; j < col_num - 1; j++) {
+            tmp -= A[i][j] * ret[j];
+        }
+        tmp /= A[i][i];
+        ret[i] = tmp;
+    }
+    var str = "(";
+    for (i = 0; i < ret.length; i++) {
+        str += ret[i];
+        if (i < ret.length - 1) str += ",";
+        else str += ")";
+    }
+    WScript.Echo("解：");
+    WScript.Echo(str);
+    return ret;
 }
 
 // 要素に数値が入った行列(2次元配列)を受け取って表示する関数
@@ -111,3 +106,16 @@ function row_add(A,a,u,v){
 
 /*【10】ここに行を交換する関数の定義を書く（コピペする）*/
 
+// 行を交換する関数
+function row_swap(A, i, j) {
+    var temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+
+// 行基本変形を行う関数
+function row_add(A, factor, src, dest) {
+    for (var k = 0; k < A[src].length; k++) {
+        A[dest][k] += factor * A[src][k];
+    }
+}
